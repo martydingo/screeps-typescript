2eb1a3d5dae702e6e6146feeec15ccddca6a770c
martydingo <marty@dingopaws.net>
2024-01-14 15:11:12 +0000

    Added Verbosity

--- src/lib/utils/log.ts
+++ src/lib/utils/log.ts
@@ -1,5 +1,8 @@
+import { config } from "config/config"
+
 export const log = {
     debug(message: any) {
+        if(config.logging.verbosity > 1) return
         console.log(`[LOG] ${JSON.stringify(message, null, 2)}`)
     }
 }
