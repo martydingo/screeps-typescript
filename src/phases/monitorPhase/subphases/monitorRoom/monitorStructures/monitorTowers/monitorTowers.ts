da75473ec5bb778a25c3f2cdd048b311184c069c
Martin George <marty@dingo.foo>
2024-01-22 16:53:10 +0000

    commit

--- src/phases/monitorPhase/subphases/monitorRoom/monitorStructures/monitorTowers/monitorTowers.ts
+++ src/phases/monitorPhase/subphases/monitorRoom/monitorStructures/monitorTowers/monitorTowers.ts
@@ -1,17 +1,16 @@
-function buildTowerMonitorMemory(roomName: string){
-    if(!Memory.rooms[roomName].monitoring.structures.towers){
+function buildTowerMonitorMemory(roomName: string) {
+    if (!Memory.rooms[roomName].monitoring.structures.towers) {
         Memory.rooms[roomName].monitoring.structures.towers = {}
     }
 }
 
-export function monitorTowers(roomName: string){
+export function monitorTowers(roomName: string) {
     buildTowerMonitorMemory(roomName)
     const towers = Object.values(Game.structures).filter(structure => structure.structureType === STRUCTURE_TOWER && structure.room.name === roomName) as StructureTower[]
     towers.forEach(tower => {
-        if(!Memory.rooms[roomName].monitoring.structures.towers[tower.id]){
-            Memory.rooms[roomName].monitoring.structures.towers[tower.id] = {
-                energy: tower.store[RESOURCE_ENERGY]
-            }
+        Memory.rooms[roomName].monitoring.structures.towers[tower.id] = {
+            energy: tower.store[RESOURCE_ENERGY]
+
         }
     })
 
